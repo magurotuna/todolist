e3bd598063d28c77bff9a21436ac1b55d5bf21ff
Yusuke Tanaka <yusuke-t@g.ecc.u-tokyo.ac.jp>
2017-03-09 07:45:06 +0900

    herokuのMongoDBに対応

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 var model = require('../model');
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/db');
+//mongoose.connect('mongodb://localhost/db');
+mongoose.connect('mongodb://heroku_5kk5kw4b:g5nqivufg5ubrlkv72gps78hpe@ds125060.mlab.com:25060/heroku_5kk5kw4b')
 //var Task = model.Task;
 
 /* GET home page. */
